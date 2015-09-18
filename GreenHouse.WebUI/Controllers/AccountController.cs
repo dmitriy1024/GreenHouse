7596051a9c256bbdb9a0891135a43a6f6ddf0080
dmitriy1024 <dfedoryshyn@gmail.com>
2015-09-18 02:54:39 +0200

    added registration with name, bugs with adding fixed

--- GreenHouse.WebUI/Controllers/AccountController.cs
+++ GreenHouse.WebUI/Controllers/AccountController.cs
@@ -152,7 +152,8 @@ namespace GreenHouse.WebUI.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                string userName = String.Format("{0} {1}", model.FName, model.LName);
+                var user = new ApplicationUser { UserName = userName, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
