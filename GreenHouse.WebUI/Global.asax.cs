3607380e50d8cb9c402b11895133490243706b8c
dmitriy1024 <dfedoryshyn@gmail.com>
2015-09-27 17:59:11 +0300

    Admin and admin role adding to db were made

--- GreenHouse.WebUI/Global.asax.cs
+++ GreenHouse.WebUI/Global.asax.cs
@@ -1,4 +1,6 @@
 ﻿using GreenHouse.WebUI.Infrastructure;
+using GreenHouse.WebUI.Models;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +8,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace GreenHouse.WebUI
 {
@@ -19,6 +23,23 @@ namespace GreenHouse.WebUI
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
+
+            using (var context = new ApplicationDbContext())
+            {
+                if (context.Roles.Where(a => String.Compare(a.Name, "admin", true) == 0).Count() == 0)
+                {
+                    context.Roles.Add(new IdentityRole("admin"));
+                    context.SaveChanges();
+                }            
+                
+                var um = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+                var user = new ApplicationUser() { UserName = "Admin", Email = "admin@gmail.com" };
+                var createResult = um.Create(user, "Aa123!");
+                if(createResult.Succeeded)
+                {
+                    var roleresult = um.AddToRole(user.Id, "admin");
+                }                                                
+            }
         }
     }
 }
