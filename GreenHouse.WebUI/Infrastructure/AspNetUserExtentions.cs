d4001e5f6106eedac7a707b1f64cbb34b880ca17
dmitriy1024 <dfedoryshyn@gmail.com>
2015-09-28 17:04:46 +0300

    bug with aspNetUserExt was fixed

---
+++ GreenHouse.WebUI/Infrastructure/AspNetUserExtentions.cs
@@ -0,0 +1,19 @@
+﻿using GreenHouse.Domain.Entities;
+using GreenHouse.WebUI.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GreenHouse.WebUI.Infrastructure
+{
+    static public class AspNetUserExtentions
+    {
+        public static bool IsAdmin(this AspNetUser user)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+                return userManager.IsInRole(user.Id, "admin");
+            }
+        }
+    }
+}
