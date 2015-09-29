2d3e895c819e542313672fbd225a112f416259bb
dmitriy1024 <dfedoryshyn@gmail.com>
2015-09-29 11:22:09 +0300

    bug after merging was fixed

--- GreenHouse.WebUI/Infrastructure/AspNetUserExtentions.cs
+++ GreenHouse.WebUI/Infrastructure/AspNetUserExtentions.cs
@@ -9,10 +9,6 @@ namespace GreenHouse.WebUI.Infrastructure
     {
         public static bool IsAdmin(this AspNetUser user)
         {
-<<<<<<< HEAD
-            var i = 9;
-=======
->>>>>>> 2a6f835f6074e3c27c6043e47c9f456b2de536f0
             using (var context = new ApplicationDbContext())
             {
                 var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
